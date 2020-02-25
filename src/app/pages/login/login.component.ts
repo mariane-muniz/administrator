c00d2d338ab789f4bcd8974ceebc924a40cf5895
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-02-25 10:54:28 -0300

    Removendo arquivos nao utilizados

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,34 +9,8 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  form: FormGroup;
-  message: string;
-  username: FormControl;
-  password: FormControl;
-  rememberMe: boolean;
-  valid: boolean = true;
-
   constructor(private route: Router) { }
 
   ngOnInit() {
-    this.createForm();
-  }
-
-  private createForm(): void {
-    this.username = new FormControl('', Validators.required);
-    this.password = new FormControl('', Validators.required);
-    this.form = new FormGroup({
-      username: this.username,
-      password: this.password
-    });
-  }
-
-  onSubmit(): void {
-    console.log('Send form request.', JSON.stringify(this.username));
-    if (this.valid) {
-      console.info('Valid form');
-    } else {
-      console.log('invalid form');
-    }
   }
 }
